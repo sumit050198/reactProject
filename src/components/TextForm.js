da44a4a481cf708d09f36fc95c497dd8aaa6d52b
sumit050198 <sumitpandey050198@gmail.com>
2024-05-15 12:48:38 +0530

    second commit

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -36,8 +36,8 @@ const clearToClick = () => {
       <div className="mb-3">
         <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor: props.mode==='dark'?'gray':'white', color: props.mode === 'dark'?'white':'black'}} id="myBox" rows="6"></textarea>
       </div>
-      <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to Uppercase</button>
-      <button className="btn btn-primary" onClick={handleLoClick}>Convert to Lowercase</button>
+      <button className="btn btn-primary mx-0" onClick={handleUpClick}>Convert to Uppercase</button>
+      <button className="btn btn-primary mx-2 my-2" onClick={handleLoClick}>Convert to Lowercase</button>
       <button className="btn btn-primary mx-2" onClick={clearToClick}>Clear Text</button>
       <button className="btn btn-primary mx-2 my-2" onClick={handleToCopy}>Copy Text</button>
       <div className="container my-3" style={{color: props.mode ==='dark'?'white':'black'}}>
