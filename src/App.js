d57dfa7e12ac4044d4d709189a861397bc49a9c6
sumit050198 <sumitpandey050198@gmail.com>
2024-05-15 12:29:31 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
+// import AboutUs from './components/AboutUs';
+import React, { useState } from 'react';
+
 
 function App() {
+  const [mode, setMode] = useState('light');  //whether dark mode is enable or not
+
+  const toggleMode = () =>{
+    if(mode === 'light'){
+      setMode('dark'); 
+      document.body.style.backgroundColor = 'gray'
+    }
+    else{
+      setMode('light');
+      document.body.style.backgroundColor = 'white'
+
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <>
+  <Navbar title="Logo" mode={mode} toggleMode={toggleMode}/>
+  {/* <Navbar/> */}
+  <div className="container">
+  <TextForm heading="Enter the text to analyze below" mode={mode}/>
+    {/* <AboutUs/> */}
+  </div>
+  </>
   );
 }
 
---
+++ src/components/TextForm.js
@@ -0,0 +1,52 @@
+
+
+
+import React, { useState } from 'react';
+
+export default function TextForm(props) {
+  const [text, setText] = useState('');
+
+  const handleUpClick = () => {
+    let newText = text.toUpperCase();
+    setText(newText);
+  }
+
+  const handleLoClick = () => {
+    let newText = text.toLowerCase();
+    setText(newText);
+  }
+
+const clearToClick = () => {
+  let newText = '';
+  setText(newText);
+}
+  const handleToCopy = () => {
+    let text = document.getElementById("myBox");
+    text.select();
+    navigator.clipboard.writeText(text.value);
+
+  }
+  const handleOnChange = (event) => {
+    setText(event.target.value);
+  }
+
+  return (
+    <div className="container" style={{color: props.mode === 'dark'?'white':'black'}}>
+      <h1 className="my-3">{props.heading}</h1>
+      <div className="mb-3">
+        <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor: props.mode==='dark'?'gray':'white', color: props.mode === 'dark'?'white':'black'}} id="myBox" rows="6"></textarea>
+      </div>
+      <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to Uppercase</button>
+      <button className="btn btn-primary" onClick={handleLoClick}>Convert to Lowercase</button>
+      <button className="btn btn-primary mx-2" onClick={clearToClick}>Clear Text</button>
+      <button className="btn btn-primary mx-2 my-2" onClick={handleToCopy}>Copy Text</button>
+      <div className="container my-3" style={{color: props.mode ==='dark'?'white':'black'}}>
+        <h1>Your text summary</h1>
+        <p>{text.split(" ").length} words and {text.length} characters</p>
+        <h2>Preview</h2>
+        <p>{text.length>0?text:"Enter sonething in the text to preview it hear"}</p>
+      </div>
+    </div>
+  )
+}
+
